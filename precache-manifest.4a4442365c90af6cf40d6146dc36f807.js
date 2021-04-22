132c22997b09e35827fad0dabbe00b9f462af42c
SantiUriguen <santiuriguen@hotmail.com>
2021-04-22 08:59:56 -0300

    Updates

---
+++ precache-manifest.4a4442365c90af6cf40d6146dc36f807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b298d6b811250f89892ec34a06dca7b",
+    "url": "/Tabla-Periodica/index.html"
+  },
+  {
+    "revision": "8cdeff15c9507b47f8fd",
+    "url": "/Tabla-Periodica/static/css/main.e3070b86.chunk.css"
+  },
+  {
+    "revision": "6b8dc912edce1cb3e0ff",
+    "url": "/Tabla-Periodica/static/js/2.3e2e52e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tabla-Periodica/static/js/2.3e2e52e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cdeff15c9507b47f8fd",
+    "url": "/Tabla-Periodica/static/js/main.87834f50.chunk.js"
+  },
+  {
+    "revision": "f80cbf5da9a4a2e18440",
+    "url": "/Tabla-Periodica/static/js/runtime-main.777d858e.js"
+  }
+]);
